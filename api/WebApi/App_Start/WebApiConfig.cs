4eb4a9840c06e01d406ab5003a211a47dd94e813
shariahstandards <webmaster@shariahstandards.org>
2016-09-18 22:52:05 +0100

    sample api app

---
+++ api/WebApi/App_Start/WebApiConfig.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Http;
+using System.Web.Http.Cors;
+using Owin;
+
+namespace WebApi
+{
+    public class WebApiConfig
+    {
+        public static void Configure(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+            var cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new {id = RouteParameter.Optional});
+
+            app.UseWebApi(config);
+        }
+    }
+}
---
+++ api/WebApi/UserProfile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApi
+{
+    public class UserProfile
+    {
+        public string Name { get; set; }
+        public string Picture { get; set; }
+        public string user_id { get; set; }
+    }
+}
