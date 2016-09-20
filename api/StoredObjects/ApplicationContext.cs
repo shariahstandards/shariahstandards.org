6e53f7a00a17cdb773fc0ee20ffdf737329f1f35
shariahstandards <webmaster@shariahstandards.org>
2016-09-20 21:54:09 +0100

    api wip

---
+++ api/StoredObjects/ApplicationContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoredObjects
+{
+    public class ApplicationContext:DbContext
+    {
+        public ApplicationContext():base("DefaultConnection")
+        {
+            this.Configuration.LazyLoadingEnabled = true;
+        }
+        public IDbSet<Auth0User> Users { get; set; } 
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Auth0User>().HasKey(x => x.Id);
+            modelBuilder.Entity<Auth0User>().Property(x => x.Name).IsRequired().HasMaxLength(100);
+            modelBuilder.Entity<Auth0User>().Property(x => x.PictureUrl).IsRequired().HasMaxLength(1000);
+        }
+    }
+}
---
+++ api/StoredObjects/Auth0User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoredObjects
+{
+    public class Auth0User
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string PictureUrl { get; set; }
+    }
+}
