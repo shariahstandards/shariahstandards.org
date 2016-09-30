b043d0eada07dbe9fe7d044698dc7e3e15b40b78
shariahstandards <webmaster@shariahstandards.org>
2016-09-30 20:05:31 +0100

    some cosmetic changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { HttpModule }       from '@angular/http'
 // import {  } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { ChartsModule } from 'ng2-charts/ng2-charts';
+// import { ChartsModule } from 'ng2-charts/ng2-charts';
 import { ShariahStandardsAppComponent } from './shariah-standards.component';
 import { ZakatCalculatorComponent } from './zakat-calculator/zakat-calculator.component';
 import { InheritanceCalculatorComponent } from './inheritance-calculator/inheritance-calculator.component';
@@ -15,7 +15,7 @@ import { QuranSearchComponent } from './quran-search/quran-search.component';
 import { PrayerTimesComponent } from './prayer-times/prayer-times.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 
-import {AlertModule, DatepickerModule} from 'ng2-bootstrap/ng2-bootstrap';
+//import {AlertModule, DatepickerModule} from 'ng2-bootstrap/ng2-bootstrap';
 import {routing,appRoutingProviders} from './routes';
 import { Routes, RouterModule,RouterLinkActive,RouterLink} from '@angular/router';
 import { ArabicKeyboardComponent } from './arabic-keyboard/arabic-keyboard.component';
@@ -39,8 +39,8 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap'
     HttpModule,
     CommonModule,
     FormsModule,
-    ChartsModule,
-    DatepickerModule,
+    // ChartsModule,
+    //DatepickerModule,
     NgbModule,
     routing
   ],
